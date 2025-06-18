65adf77d287a5b95c55b4873c6d76ba3540b00bb
Parsa_Esfandiari <esfandiary4242@gmail.com>
2025-06-18 15:23:14 +0330

    Refactor API route files for improved code formatting and readability, including consistent spacing and structure in student and teacher GET methods.

--- src/app/api/student/[id]/route.js
+++ src/app/api/student/[id]/route.js
@@ -5,20 +5,19 @@ import { NextResponse } from "next/server";
 // اتصال به دیتابیس
 await dbConnect();
 
-export async function GET(req,{params}) {
+export async function GET(req, { params }) {
   // گرفتن آیدی کاربر
-    const id = params.id;
-    try {
-
-      // دریافت کاربران از دیتابیس
-      const user = await User.findOne({_id:id}); 
-      // بازگرداندن پاسخ موفقیت‌آمیز
-      return NextResponse.json({user}, { status: 200 });
-    } catch (error) {
-      console.error("Error fetching user:", error);
-      return NextResponse.json(
-        { error: "Failed to fetch user" },
-        { status: 500 }
-      );
-    }
+  const id = params.id;
+  try {
+    // دریافت کاربران از دیتابیس
+    const user = await User.findOne({ _id: id });
+    // بازگرداندن پاسخ موفقیت‌آمیز
+    return NextResponse.json({ user }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch user" },
+      { status: 500 }
+    );
   }
+}
