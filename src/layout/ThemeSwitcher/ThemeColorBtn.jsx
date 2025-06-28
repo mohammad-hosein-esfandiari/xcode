4ca87ee5279b063aafd818088e45ae0a96461d36
Parsa_Esfandiari <esfandiary4242@gmail.com>
2025-06-28 20:33:44 +0330

    Refactor ThemeBtnHolder and ThemeColorBtn components for improved code formatting and consistency

--- src/layout/ThemeSwitcher/ThemeColorBtn.jsx
+++ src/layout/ThemeSwitcher/ThemeColorBtn.jsx
@@ -1,11 +1,9 @@
-'use client'
+"use client";
 import ThemeRadio from "./ThemeRadio";
 const ThemeColorBtn = () => {
-
   return (
     <div className="flex flex-col items-center mt-2">
       <ThemeRadio
-        
         id="blue"
         name="a"
         color="checked:bg-[#525fe1] outline-[#525fe1]"
