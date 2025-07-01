5afc026492b49255454135eeb30195b45494caa6
Parsa_Esfandiari <esfandiary4242@gmail.com>
2025-07-01 13:45:37 +0330

    code cleaning

--- src/middleware.js
+++ src/middleware.js
@@ -4,26 +4,25 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   const { cookies, url } = req;
 
-    let token = cookies.get("UoXa-I")?.value;
-    if (token) token = JSON.parse(token);
+  let token = cookies.get("UoXa-I")?.value;
+  if (token) token = JSON.parse(token);
 
-    if (url.includes("auth") && token) {
-      const redirect_url = new URL("/student-panel/dashboard", url);
-      return NextResponse.redirect(redirect_url);
-    }
-    if (url.includes("panel") && !token) {
-      const redirect_url = new URL("/auth/login", url);
-      return NextResponse.redirect(redirect_url);
-    }
-    if (url.includes("pricing") && !token) {
-      const redirect_url = new URL("/auth/login", url);
-      return NextResponse.redirect(redirect_url);
-    }
-    if (url.includes("video") && !token) {
-      const redirect_url = new URL("/auth/login", url);
-      return NextResponse.redirect(redirect_url);
-    }
-  
+  if (url.includes("auth") && token) {
+    const redirect_url = new URL("/student-panel/dashboard", url);
+    return NextResponse.redirect(redirect_url);
+  }
+  if (url.includes("panel") && !token) {
+    const redirect_url = new URL("/auth/login", url);
+    return NextResponse.redirect(redirect_url);
+  }
+  if (url.includes("pricing") && !token) {
+    const redirect_url = new URL("/auth/login", url);
+    return NextResponse.redirect(redirect_url);
+  }
+  if (url.includes("video") && !token) {
+    const redirect_url = new URL("/auth/login", url);
+    return NextResponse.redirect(redirect_url);
+  }
 
   return NextResponse.next();
 }
